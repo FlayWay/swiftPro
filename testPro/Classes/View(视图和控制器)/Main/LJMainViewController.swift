0fe3b53e763d9087269328d2e5e5c4ec8dfb215a
sunshiyuan <shiyuan.sun@trustway.cn>
2018-06-26 17:58:47 +0800

    设置文件目录和启动文件

---
+++ testPro/Classes/View(视图和控制器)/Main/LJMainViewController.swift
@@ -0,0 +1,19 @@
+//
+//  LJMainViewController.swift
+//  testPro
+//
+//  Created by ljkj on 2018/6/26.
+//  Copyright © 2018年 ljkj. All rights reserved.
+//
+
+import UIKit
+
+class LJMainViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
