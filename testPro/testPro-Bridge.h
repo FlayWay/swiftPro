bb23d0883adfe5902eb54d5dff81e3e3ab516d62
sunshiyuan <shiyuan.sun@trustway.cn>
2018-06-27 10:08:50 +0800

    添加桥接文件使swift项目能够调用oc方法

---
+++ testPro/testPro-Bridge.h
@@ -0,0 +1,16 @@
+//
+//  testPro-Bridge.h
+//  testPro
+//
+//  Created by ljkj on 2018/6/26.
+//  Copyright © 2018年 ljkj. All rights reserved.
+//  桥接文件 专门用于引入 OC 的头文件，一旦引入，swift 就可以正常使用(宏定义除外)
+//  不能提示
+#ifndef testPro_Bridge_h
+#define testPro_Bridge_h
+
+#import "CZAdditions.h"
+
+
+
+#endif /* testPro_Bridge_h */
