c998d0825223f911e2e08da18c56cf99cc263d34
sunshiyuan <shiyuan.sun@trustway.cn>
2018-06-28 16:56:46 +0800

    tableview

--- testPro/Classes/View(视图和控制器)/Main/LJBaseViewController.swift
+++ testPro/Classes/View(视图和控制器)/Main/LJBaseViewController.swift
@@ -36,10 +36,17 @@ extension LJBaseViewController {
     
     private func setupUI(){
         view.backgroundColor = UIColor.cz_random()
-        
         setupNavgationBar()
+        setupTableView()
     }
     
+    
+    /// 设置表格视图
+    private func setupTableView(){
+        
+        tableView = UITableView(frame: view.bounds, style: .plain)
+        view.insertSubview(tableView!, belowSubview:navigationBar)
+    }
     
     /// 设置导航条
     private func setupNavgationBar(){
