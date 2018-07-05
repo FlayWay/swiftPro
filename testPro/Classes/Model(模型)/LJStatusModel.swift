dfbeffa87820efc1498835a1024fde860aef078c
sunshiyuan <shiyuan.sun@trustway.cn>
2018-07-05 13:54:06 +0800

    建立数据模型

---
+++ testPro/Classes/Model(模型)/LJStatusModel.swift
@@ -0,0 +1,27 @@
+//
+//  LJStatusModel.swift
+//  testPro
+//
+//  Created by ljkj on 2018/7/5.
+//  Copyright © 2018年 ljkj. All rights reserved.
+//
+
+import UIKit
+import YYModel
+
+class LJStatusModel: NSObject {
+
+    // 基本数据类型 赋0
+    // int 类型 在64位机器中是64位 在32位中是32位
+    // 如果不写Int64 在5c/4s/ipad2 上无法运行
+    var id:Int64 = 0
+    // 微博信息内容
+    var text:String?
+    
+    // 计算型属性
+    override var description: String{
+        
+        return yy_modelDescription()
+    }
+    
+}
