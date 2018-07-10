6d5d9b488538e9242e62d2568c1c2d3a75007805
sunshiyuan <shiyuan.sun@trustway.cn>
2018-07-10 16:57:15 +0800

    加载授权页面

--- testPro/Classes/Tools/LJCommon.swift
+++ testPro/Classes/Tools/LJCommon.swift
@@ -9,6 +9,13 @@
 import Foundation
 
 
+/// 应用程序ID
+let LJAppKey = "3145109093"
+/// 应用程序加密信息
+let LJAppSecret = "8192dc0a63798f3e8a969f5302b5ee49"
+/// 应用程序回调地址
+let ljAppDirectUrl = "http://www.baidu.com"
+
 /// 用户登录需要通知
 let LJUserShouldloginNotification = "LJUserShouldloginNotification"
- 
+
--- testPro/Classes/View(视图和控制器)/Main/OAuth/LJOAuthViewController.swift
+++ testPro/Classes/View(视图和控制器)/Main/OAuth/LJOAuthViewController.swift
@@ -12,26 +12,31 @@ class LJOAuthViewController: UIViewController {
 
     private lazy var webView = UIWebView()
     
+    override func loadView() {
+        
+        view = webView
+        view.backgroundColor = UIColor.white
+        title = "登录app"
+        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "返回",  target: self, action: #selector(close), isBackButton: true)
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-       view = webView
-        view.backgroundColor = UIColor.white
-        title = "登录app"
-        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "返回",  target: self, action: #selector(close), isBackButton: true)
-        
-        
+      let urlString = "https://api.weibo.com/oauth2/authorize?client_id=\(LJAppKey)&redirect_uri=\(ljAppDirectUrl)"
+        guard let url = URL(string: urlString) else {
+            return
+        }
+        let request = URLRequest(url: url)
+        webView.loadRequest(request)
     }
 
 }
-
 
 // MARK: - 监听方法
 extension LJOAuthViewController {
     
    @objc func close() {
-    
         dismiss(animated: true, completion: nil)
     }
     
